90b288e6c6333b4548b0c9805b86611c028963c4
sleekEagle <lahirunuwanisme@gmail.com>
2026-07-11 12:13:07 +0530

    updates

--- analyze.py
+++ analyze.py
@@ -73,20 +73,24 @@ def dataset_curves(dataset, model, method):
             L = video.size(2)
             idx = list(range(L))
 
+            if method in ['greedy','foolish','brute']:
+                o_logits = data['full']['logits']
+                o_sm = F.softmax(torch.tensor(o_logits[None,:]), dim=1)
+
+                i_logits = []
+                for i in range(L):
+                    i_logits.append(data[str(i)]['logits'][None,:])
+                i_logits = np.concatenate(i_logits)
+                i_logits = torch.tensor(i_logits)
+                sm = F.softmax(i_logits, dim=1)
+                js = jensen_shannon(sm, o_sm.repeat(16,1))
+                idx = torch.argsort(js)
+
             if method == 'random':
                 random.shuffle(idx)
             elif method in ['greedy','foolish']:
-                o_logits = data['full']['logits']
-                o_cls = np.argmax(o_logits)
-
-                max_l_list = []
-                for i in range(L):
-                    max_l_list.append(data[str(i)]['logits'][o_cls])
-                max_l_list = np.array(max_l_list)
-                idx = np.argsort(-1*max_l_list)
-
                 if method == 'foolish':
-                    idx = np.argsort(max_l_list)
+                    idx = np.argsort(-1*js)
             elif method == 'facility': # facility location
                 emb = []
                 for i in range(L):
@@ -94,18 +98,11 @@ def dataset_curves(dataset, model, method):
                 emb = np.concatenate(emb, axis=0)
                 idx = func.emb_facilitylocation(emb)
             elif method == 'brute':
-                o_logits = data['full']['logits']
-                o_cls = np.argmax(o_logits)
-                max_l_list = []
-                for i in range(L):
-                    max_l_list.append(data[str(i)]['logits'][o_cls])
-                max_l_list = np.array(max_l_list)
-                best_idx = np.argmax(max_l_list)
-
-                def brute(video, best_idx, model):
-                    def get_best_idx(model, video, idx_present, o_cls):
+                best_idx = torch.argmin(js)
+                def brute(video, best_idx, model, o_sm):
+                    def get_best_idx(model, video, idx_present, o_sm):
                         idx_left = list(set(range(video.size(2)))-set(idx_present))
-                        pred_logits = torch.empty(0).to(model.device)
+                        pred_sm_ar = torch.empty(0).to(model.device)
                         for idx in idx_left:
                             keep = idx_present + [idx]
                             tofill, fillwith = func.past_fill(keep)
@@ -113,20 +110,22 @@ def dataset_curves(dataset, model, method):
                             func.fill_video(tofill, fillwith, fvideo)
 
                             pred = model.predict_video(fvideo)
-                            l = pred[:,o_cls]
-                            pred_logits = torch.concatenate([pred_logits, l])
-                        bi = idx_left[torch.argmax(pred_logits)]
+                            pred_sm = F.softmax(pred,dim=1)
+                            pred_sm_ar = torch.concatenate([pred_sm_ar, pred_sm])
+
+                        js = jensen_shannon(pred_sm_ar.to(o_sm.device), o_sm.repeat(pred_sm_ar.size(0),1))
+                        bi = idx_left[torch.argmin(js)]
                         return bi
                     
                     sel_idx = [int(best_idx)]
                     for _ in range(video.size(2)-2):
-                        bi = get_best_idx(model, video, sel_idx, o_cls)
+                        bi = get_best_idx(model, video, sel_idx, o_sm)
                         sel_idx += [bi]
                     sel_idx += list(set(range(video.size(2))) - set(sel_idx))
 
                     return sel_idx
                 
-                idx = brute(video, best_idx, model) 
+                idx = brute(video, best_idx, model, o_sm) 
 
             sim_ar, js_ar = get_video_curve(model, video, data, idx)
             d={
@@ -135,82 +134,5 @@ def dataset_curves(dataset, model, method):
 
             func.save_dict_to_h5(f, d)
 
-
-def dataset_curves_debug(dataset, model, method):
-    level1_file = os.path.join(CONF.LEVEL_1_PATH, f'{dataset}_{model}.h5')
-    out_path = CONF.OUT_PATH
-    out_file = os.path.join(out_path, method)
-    os.makedirs(out_file, exist_ok=True)
-    out_file = os.path.join(out_file, f'curves_{dataset}_{model}.h5') 
-
-    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
-    model = get_model.get_model(dataset, model)
-
-    for i in range(len(path_list)):
-        print(f'{i} of {len(path_list)} is done.', end='\r')
-
-        video = model.get_video(path_list[i])
-        fname = os.path.basename(path_list[i])
-        data = func.get_h5_item(level1_file, fname)
-        L = video.size(2)
-        idx = list(range(L))
-
-        if method in ['greedy','foolish','brute']:
-            o_logits = data['full']['logits']
-            o_sm = F.softmax(torch.tensor(o_logits[None,:]), dim=1)
-
-            i_logits = []
-            for i in range(L):
-                i_logits.append(data[str(i)]['logits'][None,:])
-            i_logits = np.concatenate(i_logits)
-            i_logits = torch.tensor(i_logits)
-            sm = F.softmax(i_logits, dim=1)
-            js = jensen_shannon(sm, o_sm.repeat(16,1))
-            idx = torch.argsort(js)
-
-        if method == 'random':
-            random.shuffle(idx)
-        elif method in ['greedy','foolish']:
-            if method == 'foolish':
-                idx = np.argsort(-1*js)
-        elif method == 'facility': # facility location
-            emb = []
-            for i in range(L):
-                emb.append(data[str(i)]['feat'][None,:])
-            emb = np.concatenate(emb, axis=0)
-            idx = func.emb_facilitylocation(emb)
-        elif method == 'brute':
-            best_idx = torch.argmin(js)
-            def brute(video, best_idx, model, o_sm):
-                def get_best_idx(model, video, idx_present, o_sm):
-                    idx_left = list(set(range(video.size(2)))-set(idx_present))
-                    pred_sm_ar = torch.empty(0).to(model.device)
-                    for idx in idx_left:
-                        keep = idx_present + [idx]
-                        tofill, fillwith = func.past_fill(keep)
-                        fvideo = video.clone()
-                        func.fill_video(tofill, fillwith, fvideo)
-
-                        pred = model.predict_video(fvideo)
-                        pred_sm = F.softmax(pred,dim=1)
-                        pred_sm_ar = torch.concatenate([pred_sm_ar, pred_sm])
-
-                    js = jensen_shannon(pred_sm_ar.to(o_sm.device), o_sm.repeat(pred_sm_ar.size(0),1))
-                    bi = idx_left[torch.argmin(js)]
-                    return bi
-                
-                sel_idx = [int(best_idx)]
-                for _ in range(video.size(2)-2):
-                    bi = get_best_idx(model, video, sel_idx, o_sm)
-                    sel_idx += [bi]
-                sel_idx += list(set(range(video.size(2))) - set(sel_idx))
-
-                return sel_idx
-            
-            idx = brute(video, best_idx, model, o_sm) 
-
-        sim_ar, js_ar = get_video_curve(model, video, data, idx)
-        pass
-
 if __name__ == "__main__":
-    dataset_curves_debug('ucf101', 'mc3-18', 'random')
+    dataset_curves('ucf101', 'mc3-18', 'random')
