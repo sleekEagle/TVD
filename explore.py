91beb44926ee539b99215d01b7287a8efb8e8c70
sleekEagle <lahirunuwanisme@gmail.com>
2026-07-12 16:34:53 +0530

    plot js seq plots

--- explore.py
+++ explore.py
@@ -13,6 +13,52 @@ import CONF
 from sklearn.manifold import TSNE
 import torch
 import matplotlib.pyplot as plt
+import analyze
+
+def plot_JS_seq(dataset, model_name, fname):
+    out_path = os.path.join(CONF.OUT_PATH, 'plots', 'JS_seq')
+    os.makedirs(out_path, exist_ok=True)
+    out_path_plot = os.path.join(out_path, f'{dataset}_{model_name}_{fname}.png')
+
+    level1_file = os.path.join(CONF.LEVEL_1_PATH, f'{dataset}_{model_name}.h5')
+    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
+    path = [pl for pl in path_list if os.path.basename(pl) == fname][0]
+    model = get_model.get_model(dataset, model_name)
+
+    data = func.get_h5_item(level1_file, fname)
+    video = model.get_video(path)
+    L = video.size(2)
+    js = func.get_js_video(data)
+
+    # lets compare greedy vs brute in the embedding space
+    # greedy
+    greedy_idx = torch.argsort(js)
+
+    # brute
+    best_idx = torch.argmin(js)
+    o_logits = data['full']['logits']
+    o_sm = F.softmax(torch.tensor(o_logits[None,:]), dim=1)
+    brute_idx = func.brute(video, best_idx, model, o_sm) 
+
+    assert brute_idx[0]==greedy_idx[0], 'brute and greedy does not match!'
+
+    def normalize(ar):
+        ar=np.array(ar)
+        ar = (ar-ar.min())/(ar.max()-ar.min())
+        return ar
+    
+    greedy_seq = analyze.get_video_curve(model, video, data, greedy_idx)[1]
+    brute_seq = analyze.get_video_curve(model, video, data, brute_idx)[1]
+
+    plt.figure(figsize=(8, 6))   
+    plt.plot(greedy_seq, label='Greedy')
+    plt.plot(brute_seq, label= 'Brute')
+    plt.legend()
+    plt.xlabel('Frame Number')
+    plt.ylabel('JS Div')
+    plt.savefig(out_path_plot, dpi=300)
+    plt.show()
+    pass
 
 
 def plot_point_traj(dataset, model, fname):
@@ -24,43 +70,51 @@ def plot_point_traj(dataset, model, fname):
     data = func.get_h5_item(level1_file, fname)
     video = model.get_video(path)
     L = video.size(2)
+    js = func.get_js_video(data)
+
+    # get point sizes based on JS
+    js_ = js
+    js_scaled = (js_-js_.min())/(js_.max()-js_.min())
+    point_sizes = (((300-20)*(1-js_scaled)+20))**1.2
+    point_sizes = point_sizes.to(torch.int32)
 
     # lets compare greedy vs brute in the embedding space
     # greedy
     greedy_idx = torch.argsort(js)
 
-    js_scaled = (js-js.min())/(js.max()-js.min())
-    point_sizes = (400-20)*(1-js_scaled)+40
-    point_sizes = point_sizes.to(torch.int32)
-
     # brute
-    js = func.get_js_emb(data)
     best_idx = torch.argmin(js)
     o_logits = data['full']['logits']
     o_sm = F.softmax(torch.tensor(o_logits[None,:]), dim=1)
     brute_idx = func.brute(video, best_idx, model, o_sm) 
+
+    assert brute_idx[0]==greedy_idx[0], 'brute and greedy does not match!'
 
     # get all individual + all embeddings (L+1). last emb is full 
     all_feat = torch.empty(0)
     for k in range(len(data.keys())-1):
         all_feat = torch.concatenate([all_feat, torch.tensor(data[str(k)]['feat'])[None,]],dim=0)
     all_feat = torch.concatenate([all_feat, torch.tensor(data['full']['feat'])[None,]],dim=0)
+    # all_feat = F.softmax(all_feat,dim=1)
 
     embeddings_np = all_feat.numpy()
-    tsne = TSNE(n_components=2, random_state=42, perplexity=4)
+    tsne = TSNE(n_components=2, random_state=42, perplexity=6)
     embeddings_2d = tsne.fit_transform(embeddings_np)
 
-    plt.figure(figsize=(8, 6))
-    plt.scatter(embeddings_2d[:-1, 0], embeddings_2d[:-1, 1], s=point_sizes)
-    plt.scatter(embeddings_2d[-1, 0], embeddings_2d[-1, 1], 
+    order = brute_idx
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.scatter(embeddings_2d[:-1, 0], embeddings_2d[:-1, 1], s=point_sizes)
+    ax.scatter(embeddings_2d[greedy_idx[0], 0], embeddings_2d[greedy_idx[0], 1], s=point_sizes[greedy_idx[0]], c='green')
+    ax.scatter(embeddings_2d[-1, 0], embeddings_2d[-1, 1], 
             s=200,            # Size of the 'x'
             c='red',          # Color
             marker='x',       # 'x' marker
             linewidth=3,      # Thickness of the 'x'
             label=f'full')
-    for i in range(embeddings_2d.shape[0]):
-        plt.annotate(str(i), (embeddings_2d[i, 0], embeddings_2d[i, 1]))
-    plt.title('Embeddings Visualization (t-SNE)')
+    # show the first greedy point
+    ax.scatter(embeddings_2d[greedy_idx[0], 0], embeddings_2d[greedy_idx[0], 1], s=200, c='green')
+    for i, seq_idx in enumerate(order):
+        plt.annotate(str(i), (embeddings_2d[seq_idx, 0], embeddings_2d[seq_idx, 1]))
     plt.show()
 
 
@@ -114,5 +168,5 @@ def js_vs_dist(dataset, model_name):
 
 
 if __name__ == "__main__":
-    # plot_point_traj('ucf101', 'r3d-18', 'v_Diving_g02_c04.avi')
-    js_vs_dist('ucf101', 'mc3-18')
+    plot_JS_seq('ucf101', 'r3d-18', 'v_ApplyLipstick_g01_c02.avi')
+    # js_vs_dist('ucf101', 'mc3-18')
