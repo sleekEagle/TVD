1ae6c431c4b18fda57b93edfff676c0ceda2507f
sleekEagle <lahirunuwanisme@gmail.com>
2026-07-16 11:44:58 +0530

    calc accuracy for each threshold

--- eval_curves.py
+++ eval_curves.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 import numpy as np
 from scipy.integrate import trapezoid
 import CONF
+from tqdm import tqdm
 
 def calc_auc(points):
     x = np.linspace(0, 1, len(points))
@@ -45,31 +46,84 @@ def eval_curves(dataset, model, method, forward):
     sim_auc_avg /= len(path_list)
     print(f'sim_auc: {sim_auc_avg}, js_auc: {js_auc_avg}')
 
-def eval_compression_quality(dataset, model, method):
-    curve_file = os.path.join(dir_path, method, f'curves_{dataset}_{model}.h5')
-    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
 
-    comp = {
+def eval_compression_quality(dataset, model, method, forward):
+    COMP = {
         1e-3: 0,
         5e-4: 0,
         1e-4: 0,
         5e-5: 0,
         1e-5: 0,
     }
+        
+    ward = 'forward' if forward else 'backward'
+    curve_file = os.path.join(dir_path, method, f'curves_{dataset}_{model}_{ward}.jsonl')
+    data = func.load_jsonl_to_dict(curve_file)
 
-    with h5py.File(curve_file, "r") as f:
-        for path in path_list:
-            fname = os.path.basename(path)
-            g = f[fname]
-            js_ar = g['js_ar'][:]
+    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
+    for path in path_list:
+        fname = os.path.basename(path)
+        g = data[fname]
+        js_ar = np.array(g['js_ar'])
 
-            for thr in comp.keys():
-                n = np.argwhere(js_ar<thr).min()
-                comp[thr] += int(n)
-    for thr in comp.keys():
-        comp[thr]/=len(path_list)
-    print(comp)
+        for thr in COMP.keys():
+            n = np.argwhere(js_ar<thr).min()
+            COMP[thr] += int(n)
+    for thr in COMP.keys():
+        COMP[thr]/=len(path_list)
+    print(COMP)
 
+def eval_acc_comp(dataset, model, method, forward):
+    COMP = {
+        1e-3: 0,
+        5e-4: 0,
+        1e-4: 0,
+        5e-5: 0,
+        1e-5: 0,
+    }
+    ACC = {
+        1e-3: 0,
+        5e-4: 0,
+        1e-4: 0,
+        5e-5: 0,
+        1e-5: 0,
+    }
+    
+    ward = 'forward' if forward else 'backward'
+    curve_file = os.path.join(dir_path, method, f'curves_{dataset}_{model}_{ward}.jsonl')
+    data = func.load_jsonl_to_dict(curve_file)
+    model = get_model.get_model(dataset, model)
+
+    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
+    correct = 0
+    for i in tqdm(range(len(path_list))):
+        path = path_list[i]
+        fname = os.path.basename(path)
+        g = data[fname]
+        js_ar = np.array(g['js_ar'])
+        idx_ar = g['idx']
+        video = model.get_video(path)
+        L = video.size(2)
+
+        for thr in COMP.keys():
+            n = np.argwhere(js_ar<thr).min()+1
+            COMP[thr] += int(n) 
+            idx_used = idx_ar[:n]
+
+            # eval
+            if len(idx_used)<L:
+                fvideo = func.fill_with_keep(idx_used, video)
+            else:
+                fvideo = video
+            pred_cls = torch.argmax(model.predict_video(fvideo))
+            if idx_list[i]==pred_cls:
+                ACC[thr] += 1
+    for thr in COMP.keys():
+        COMP[thr]/=len(path_list)
+        ACC[thr]/=len(path_list)
+
+    print(COMP)
+    print(ACC)
 
 if __name__ == "__main__":
-    eval_curves('ucf101', 'mc3-18', 'greedy', forward=False)
+    eval_acc_comp('ucf101', 'mc3-18', 'brute', forward=True)
