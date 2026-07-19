1a4b9c9f42103ebaef1e64a22d09a2b927cae626
sleekEagle <lahirunuwanisme@gmail.com>
2026-07-19 19:09:12 +0530

    updates

--- explore.py
+++ explore.py
@@ -14,6 +14,7 @@ from sklearn.manifold import TSNE
 import torch
 import matplotlib.pyplot as plt
 import analyze
+from torchvision.utils import make_grid
 
 def plot_JS_seq(dataset, model_name, fname, forward):
     out_path = os.path.join(CONF.OUT_PATH, 'plots', 'JS_seq')
@@ -156,10 +157,52 @@ def js_vs_dist(dataset, model_name):
     with open(out_path_txt, 'w') as file:
         file.write(f'Avg correlation coeff: {avg_corr}')
 
+def plot_frames(dataset, model_name, fname, forward, thr=1e-3):
+    out_path = r'D:\output\TVD\plots\frames'
+    out_path = os.path.join(out_path, f'{fname}.png')
+    path_list, cls_list, idx_list = data_paths.get_paths(dataset)
+    basenames = [os.path.basename(s) for s in path_list]
+    path_idx = basenames.index(fname)
 
-    
+    ward = 'forward' if forward else 'backward'
+    stat_path = os.path.join(CONF.OUT_PATH, 'brute' ,f'curves_{dataset}_{model_name}_{ward}.jsonl')
+    data = func.load_jsonl_to_dict(stat_path)[fname]
+    js = np.array(data['js_ar'])
+    idx = min(np.argwhere(js<thr))
+    frames = data['idx'][:int(idx[0])]
+    print(f'Frames: {frames}')
+
+    model = get_model.get_model(dataset, model_name)
+    video = model.get_video(path_list[path_idx])
+
+    grid = make_grid(video.squeeze(0).permute(1,0,2,3), nrow=video.size(2), normalize=True, pad_value=1)
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(grid.permute(1,2,0).cpu().numpy())
+    ax.axis('off')
+
+    n_frames = video.size(2)
+    frame_height = video.size(3)
+    frame_width = video.size(4)
+    for idx in frames:
+        x_center = idx * (frame_width + 2) + frame_width // 2
+        y_top = -10 # Position above the frame
+        ax.plot(x_center, y_top, marker='v', markersize=6, 
+            color='red', linestyle='none')
+        # circle = plt.Circle((x_center, y_top), radius=10, 
+        #                     color='red', fill=True, linewidth=0)
+        # ax.add_patch(circle)
+
+    plt.imshow(grid.permute(1,2,0).cpu().numpy())
+    plt.axis('off')
+    y_min, y_max = ax.get_ylim()
+    ax.set_ylim(y_min, y_max-14) 
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    plt.savefig(out_path, bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.show()
+
 
 if __name__ == "__main__":
-    plot_JS_seq('ucf101', 'mc3-18', 'v_PlayingFlute_g02_c01.avi', forward = True)
+    plot_frames('ucf101', 'mc3-18', 'v_BalanceBeam_g03_c04.avi', forward = True)
     # js_vs_dist('ucf101', 'mc3-18')
     pass
