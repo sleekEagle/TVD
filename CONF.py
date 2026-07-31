3a9aca7c9fd1f318b848b6e6c86597a4a2d12191
sleekEagle <lahirunuwanisme@gmail.com>
2026-07-31 10:04:07 +0530

    seperate diving paths for meta and data in CONF

--- CONF.py
+++ CONF.py
@@ -2,5 +2,6 @@ OUT_PATH = r"D:\output\TVD"
 SSV2_PATH = r'C:\Users\lahir\Downloads\s2s_test'
 UCF_PATH = r'c:\Users\lahir\Downloads\UCF101\UCF-101'
 UCF_SPLIT_PATH = r"D:\datasets\UCF101-splits\testlist01.txt"
-DIVING_PATH = r'D:\datasets\Diving48_rgb'
+DIVING_META_PATH = r'D:\datasets\Diving48_rgb'
+DIVING_DATA_PATH = r'D:\datasets\Diving48_rgb\rgb'
 DIVING_CKPT_PATH = r'C:\Users\lahir\code\vjepa2\ckpt'
--- dataloaders/diving48.py
+++ dataloaders/diving48.py
@@ -4,20 +4,17 @@ import json
 import CONF
 
 def get_paths():
-    data_path = CONF.DIVING_PATH
-    annot_path = os.path.join(data_path, 'Diving48_V2_test.json')
+    annot_path = os.path.join(CONF.DIVING_META_PATH, 'Diving48_V2_test.json')
 
-    with open(os.path.join(data_path, 'id2label.json'), 'r') as f:
+    with open(os.path.join(CONF.DIVING_META_PATH, 'id2label.json'), 'r') as f:
         id2label = json.load(f)
 
     paths, labels, cls_names = [], [], []
     num_samples_per_dataset = []
-    rgb_path = os.path.join(data_path, 'rgb')
     data = json.load(open(annot_path))
-    num_samples = len(data)
     num_samples_per_dataset.append(len(data))
     for d in data:
-        paths += [os.path.join(rgb_path, f'{d['vid_name']}.mp4')]
+        paths += [os.path.join(CONF.DIVING_DATA_PATH, f'{d['vid_name']}.mp4')]
         labels += [d['label']]
         cls_name = id2label[str(d['label'])]
         cls_names.append(cls_name)
