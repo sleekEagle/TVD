0335315a36a877653b5e10e6dd239dcce4fcd0d5
sleekEagle <lahirunuwanisme@gmail.com>
2026-08-09 10:03:57 +0530

    updates

--- explore.py
+++ explore.py
@@ -412,5 +412,5 @@ if __name__ == "__main__":
     # plot_multi_sfs('ssv2', 'vjepa2', False, 1035)
 
     # js_vs_dist('ucf101', 'mc3-18')
-    cls_metrics('ucf101', 'r3d-18', thr=1e-1)
+    cls_metrics('ucf101', 'r3d-18', thr=3)
     pass
